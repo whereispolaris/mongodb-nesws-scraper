3511bce0d6099e87b600fc23b9e989f1dc93b756
Santiago Enciso <whereispolaris@gmail.com>
2019-09-29 20:57:47 -0500

    Created mongoose database model and added it to server.js file.

---
+++ model/article.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+const ArticleSchema = new Schema({
+    // * Title
+    // * Summary
+    // * URL
+
+    title: {
+        type: String,
+        trim: true,
+        required: "Title is Required"
+    },
+    summary: {
+        type: String,
+        trim: true,
+        required: "Summary is Required"
+    },
+
+    url: {
+        type: String,
+        trim: true,
+        required: "URL is Required"
+    }
+});
+
+const Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Connect to the Mongo DB
+mongoose.connect(MONGODB_URI);
+
 // Database Model
+const Article = require('./model/article.js');
 
 // Initialize Express
 const app = express();
@@ -17,5 +24,4 @@ app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-
 });
